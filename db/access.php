573994fcc72c5c43716a21c3ff171abba7f88a12
stefankoegel <stefan.koegel@uni-ulm.de>
2013-10-17 12:06:32 +0200

    Removed TODO
    There was nothing left to do...

--- db/access.php
+++ db/access.php
@@ -18,28 +18,6 @@
 /**
  * Capability definitions for the groupdistribution module
  *
- * The capabilities are loaded into the database table when the module is
- * installed or updated. Whenever the capability definitions are updated,
- * the module version number should be bumped up.
- *
- * The system has four possible values for a capability:
- * CAP_ALLOW, CAP_PREVENT, CAP_PROHIBIT, and inherit (not set).
- *
- * It is important that capability names are unique. The naming convention
- * for capabilities that are specific to modules and blocks is as follows:
- *   [mod/block]/<plugin_name>:<capabilityname>
- *
- * component_name should be the same as the directory name of the mod or block.
- *
- * Core moodle capabilities are defined thus:
- *    moodle/<capabilityclass>:<capabilityname>
- *
- * Examples: mod/forum:viewpost
- *           block/recent_activity:view
- *           moodle/site:deleteuser
- *
- * The variable name for the capability definitions array is $capabilities
- *
  * @package    mod_groupdistribution
  * @copyright  2013 Stefan Koegel
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -49,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
     'mod/groupdistribution:addinstance' => array(
-        'riskbitmask' => RISK_XSS | RISK_SPAM, //TODO
+        'riskbitmask' => RISK_XSS | RISK_SPAM,
         'contextlevel' => CONTEXT_MODULE, 
         'captype' => 'write',
         'archetypes' => array(
