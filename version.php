8fa3e43029cb374fd89c5a5aeee4c42e859de2e6
stefankoegel <stefan.koegel@uni-ulm.de>
2013-11-22 10:41:32 +0100

    Update version number

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013111402;      // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2013112200;      // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2010031900;      // Requires this Moodle version
 $module->cron      = 0;               // Period for cron to check this module (secs)
 $module->component = 'mod_groupdistribution'; // To check on upgrade, that module sits in correct place
